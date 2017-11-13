2dc984ee578a1721ff0167cc68d3bf24a83ad3eb
IsraelDeorce <israel.deorce@acad.pucrs.br>
2017-11-13 17:27:31 -0200

    Continuando

--- SisOpT2_GMemoria/src/main/Pagina.java
+++ SisOpT2_GMemoria/src/main/Pagina.java
@@ -2,6 +2,28 @@ package main;
 
 public class Pagina {
 	
+	private String idPagina;
+	private int deslocamento;
 	
+	public Pagina(String idPagina, int deslocamento) {
+		this.idPagina = idPagina;
+		this.deslocamento = deslocamento;
+	}
 
+	public String getIdPagina() {
+		return idPagina;
+	}
+
+	public void setIdPagina(String idPagina) {
+		this.idPagina = idPagina;
+	}
+
+	public int getDeslocamento() {
+		return deslocamento;
+	}
+
+	public void setDeslocamento(int deslocamento) {
+		this.deslocamento = deslocamento;
+	}
+	
 }
--- SisOpT2_GMemoria/src/main/TabelaDePaginas.java
+++ SisOpT2_GMemoria/src/main/TabelaDePaginas.java
@@ -5,7 +5,21 @@ import java.util.Map;
 
 public class TabelaDePaginas {
 	
-	Map<String, Componente> tabelaDePaginas;
+    private static final class Componentes {
+
+        public 
+        public boolean bitResidencia;	//Esta na Mem. Principal ou nao?
+        public boolean referencia; 		//foi referenciada ultimamente?
+        public int contadorLRU;			//o que tiver o menor contador eh substituido
+
+        public Componentes(Integer element, Integer level) {
+        	this.contadorLRU = 0;
+            this.element = element;
+            this.level = level;
+        }
+    }
+    
+    Map<String, Componente> tabelaDePaginas;
 	
 	public TabelaDePaginas() {
 		tabelaDePaginas = new HashMap<>();
