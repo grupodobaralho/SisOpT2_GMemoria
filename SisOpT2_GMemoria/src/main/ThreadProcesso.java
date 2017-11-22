63e0107d006f2313bd32c09ecce18cbe812f5053
IsraelDeorce <israel.deorce@acad.pucrs.br>
2017-11-22 03:54:12 -0200

    Incluindo explicação do trabalho

--- SisOpT2_GMemoria/src/main/ThreadProcesso.java
+++ SisOpT2_GMemoria/src/main/ThreadProcesso.java
@@ -6,9 +6,24 @@ import java.util.Random;
  * Trabalho 2 - Gerencia de Memoria
  * 
  * @author Israel Deorce Vieira Júnior (16104279-1)
+ * @email israel.deorce@acad.pucrs.br
  * @date 21/11/2017
  * @class Sistemas Operacionais
  * @professor Avelino Zorzo
+ * 
+ *            #Resumo do Programa:
+ * 
+ *            Este programa é uma alternativa de implementação para o exercício
+ *            proposto no segundo trabalho da disciplina de Sistemas
+ *            Operacionais. O exercício envolve o desenvolvimento de um programa
+ *            que simule um gerenciador de memória utilizando técnica de
+ *            paginação, com algoritmos LRU (Menos Recentemente Utilizado) e
+ *            aleatório como algoritmos de substituição de páginas.
+ *            Desenvolvemos a solução proposta utilizando a linguagem de
+ *            programação Java orientada a objetos, com auxilio de uma estrutura
+ *            de tabela de páginas. O enunciado do trabalho, os arquivos de
+ *            teste e o artigo que descreve melhor o problema e a solução, estão
+ *            todos disponiveis neste projeto.
  * 
  */
 public class ThreadProcesso implements Runnable {
